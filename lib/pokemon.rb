7f94e714147bae863b1b8d282e0713c9460beb4b
Alexander Carlin <ahcarlin@gmail.com>
2018-10-16 18:49:49 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,7 @@ def self.save
     INSERT INTO pokemon (name, type, db) VALUES (?, ?, ?)
   SQL
   
-  db.execute(query, self.name, self.type, self.db)
+  db[:conn].execute(query, self.name, self.type, self.db)
   binding.pry
 end
 
